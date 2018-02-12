1673de55ca0b0a2a8125407a29456045c689511b
Kathy Van Stone <kvs@pobox.com>
2018-02-12 15:58:12 -0500

    Added dates, fixed simple name

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,26 +1,92 @@
-
+require 'pry'
 #
 # Takes a unicode line or lines and parses the relevant portions from it.
 #
 class Parser
 
+  KINGDOM_CODES = {
+    'H' => '\u0036thelmearc',
+    'N' => 'An Tir',
+    'X' => 'Ansteorra',
+    'R' => 'Artemisia',
+    'A' => 'Atenveldt',
+    'Q' => 'Atlantia',
+    'V' => 'Avacal',
+    'C' => 'Caid',
+    'K' => 'Calontir',
+    'D' => 'Drachenwald',
+    'm' => 'Ealdormere',
+    'E' => 'East',
+    't' => 'Gleann Abhann',
+    'L' => 'Laurel',
+    'w' => 'Lochac',
+    'S' => 'Meridies',
+    'M' => 'Middle',
+    'n' => 'Northshield',
+    'O' => 'Outlands',
+    'T' => 'Trimaris',
+    'W' => 'West'
+  }
+
+  def date_parser
+    @date_parser ||= DateParser.new
+  end
+
   def parse(lines)
-    lines.map do |l|
-      parse_line(l)
-    end
+    lines.map do |line|
+      parse_line(line)
+    end.collect!
   end
 
   protected
     def parse_line(line)
       contents = line.split('|')
-      parse_items(*contents)
+      name, date, type, ref = contents
+      date = date_parser.parse(date)
+      parse_items(name, date, type, ref) if date
     end
+
+  #
+  # Parser to convert the strong format YYYYMMK to date and kingdom
+  #
+  class DateParser
+
+    PATTERN = /^([12][0-9][0-9][0-9])([01][0-9])([A-Za-z])$/
+
+    def kingdom_map
+      @kingdom_map ||= KINGDOM_CODES
+    end
+
+    def parse(date_str)
+      match_data = PATTERN.match(date_str)
+      return nil unless match_data
+      _, year, month, kingdom_code = match_data.to_a
+      date = Date.new(year = year.to_i, month=month.to_i)
+      kingdom = kingdom_map.fetch(kingdom_code)
+      DateSource.new(date=date, kingdom=kingdom)
+    end
+  end
+
 end
 
+#
+# Parser for pull out personal names from the armorial
+#
 class PersonalParser < Parser
 
   protected
-    def parse_items(name, *rest)
-      name
+    def parse_items(name, date, *unused)
+      NameItem.new(match_name = name, date = date, name = name, owner = name)
     end
 end
+
+#
+# Structure for the parsed result
+#
+NameItem = Struct.new(:match_name, :date, :name, :owner)
+
+#
+# Date the submission was registered
+#
+DateSource = Struct.new(:date, :kingdom)
+
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
+require 'date'
 require 'parser'
+require 'name_matcher'
 
 describe 'Parser' do
 
@@ -7,7 +9,59 @@ describe 'Parser' do
     send(:parser).parse([line]).to_a[0]
   end
 
-  context "When parsing dates"
+  let (:date_source) {
+    # Must match date-code
+    DateSource.new(date = Date.new(2018, 1), kingdom = "An Tir")
+  }
+
+  let (:date_string) {
+    "201801N"
+  }
+
+  def simple_line(name, type)
+    "#{name}|#{date_string}|#{type}||(regid:10000)"
+  end
+
+  context "General case" do
+    let (:parser) {
+      GeneralParser.new
+    }
+
+    it "ignores range dates" do
+      pending "date parsing"
+      expect(parser.parse(["Test Name|201301K-201703K|N||(regid:300000"])).to be_empty
+    end
+  end
+
+  context "When parsing dates" do
+
+    let (:parser) {
+      GeneralParser.new
+    }
+
+    def parse_date(date_str)
+      parse_line("Test Name|#{date_str}|N||(regid:4000)")[1]
+    end
+
+    it "There are at least 18 kingdoms" do
+      expect(Parser::KINGDOM_CODES.length).to be >= 18
+    end
+
+    it "Takes the first four digits as the year" do
+      expect(parse_date("201703N").date.year).to eq(2017)
+    end
+
+    it "Takes the newt two digits as the month (starting at 1)" do
+      expect(parse_date("201606N").date.month).to eq(06)
+    end
+
+    Parser::KINGDOM_CODES.each do |code, kingdom|
+      it "Translates the last letter #{code} to #{kingdom}" do
+        expect(parse_date("201503#{code}").kingdom).to eq(kingdom)
+      end
+    end
+
+  end
 
   context "When searching for personal names" do
 
@@ -16,11 +70,38 @@ describe 'Parser' do
     }
 
     it "Pulls out the first element in the normal case" do
-      line = "East Kingdom|197908E|Bv|East, Kingdom of the|(regid:30175)"
-      result = parse_line(line)
-      expect(result).to eq("East Kingdom")
+      name = "John the Smith"
+      result = parse_line(simple_line(name, "N"))
+      expect(result).to eq(
+        NameItem.new(match_name = name,
+          date = date_source,
+          name = name,
+          owner = name))
     end
+
+    it "Normalizes special characters" do
+      pending "Normalization"
+      name = "Eoin Ó Briain"
+      result = parse_line(simple_line(name, "N"))
+      expect(result).to eq(
+        NameItem.new(match_name = 'Eoin O Briain',
+          name = name,
+          owner = name,
+          date = date_source))
+    end
+
+
   end
 
   context "When searching for non personal names"
+end
+
+#
+# Used to test things outside of whether it is a personal or non personal name
+#
+class GeneralParser < Parser
+
+  def parse_items(name, date, type, extra, *rest)
+    return ["{name}:{extra}", date]
+  end
 end
